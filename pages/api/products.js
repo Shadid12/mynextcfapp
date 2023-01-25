d7beb1dbd2de503c188278cf57e7abf994c709a6
Shadid12 <Shadid12@users.noreply.github.com>
2023-01-25 06:09:17 -0500

    update files

---
+++ pages/api/products.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-anonymous-default-export */
+export const config = {
+  runtime: 'experimental-edge',
+}
+ 
+export default async function (req) {
+  if(req.method === 'POST') {
+    const body = await readRequestBodyStream(req.body);
+    const { title, price, description } = JSON.parse(body);
+    // TODO: Save the product to the database
+    return new Response(
+      JSON.stringify({ 
+        message: 'Product created successfully'
+      }),
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+  }
+}
+ 
+async function readRequestBodyStream(body) {
+  const reader = body.getReader();
+  let result = new Uint8Array(0);
+ 
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) {
+        break;
+    }
+ 
+    const newResult = new Uint8Array(result.length + value.length);
+    newResult.set(result);
+    newResult.set(value, result.length);
+    result = newResult;
+  }
+  return new TextDecoder().decode(result);
+}
