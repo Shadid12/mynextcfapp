d32ca1de2c4236bc74de8fe5f3e20cee4d629783
Shadid12 <Shadid12@users.noreply.github.com>
2023-01-25 06:25:41 -0500

    update env

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import fauna from 'faunadb';
 
 const q = fauna.query;
 
-const client = new fauna.Client({ secret: process.env.FAUNA_SECRET });
+const client = new fauna.Client({ secret: 'fnAE7JV9P0ACT3La9djQtpvnOPa5MzHI_DafJtxl' });
 
 export const createProduct = async (title, price, description) => {
   const product = {
@@ -14,3 +14,14 @@ export const createProduct = async (title, price, description) => {
   };
   return client.query(q.Create(q.Collection('Product'), product));
 };
+
+export const getProducts = async () => {
+  const response = await client.query(
+    q.Map(
+      q.Paginate(q.Documents(q.Collection('Product'))),
+      q.Lambda(x => q.Get(x))
+    )
+  );
+  const products = response.data.map((product) => ({ ...product.data, _id: product.ref.id }));
+  return products;
+}
